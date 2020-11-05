0f35fd983e533711a6082be594905a79bd5bd8f6
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-11-04 21:46:10 -0500

    MVP met, additional annotation added

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,6 +5,18 @@ module.exports = {
   get,
 };
 
+function repair(item) {
+  // see Object.defineProperty() on MDN web docs page
+  // The arguments passed here to Object.defineProperty() are the object to be modified (item), the property on that object 
+  // to be defined/modified (durability), and the "descriptor" of the property (in this case, the value (100) to be assigned to the 
+  // "item" object's "durability" property).
+    Object.defineProperty(item, 'durability', {
+      value: 100
+    })
+    // return an item whose durability === 100
+    return {...item}
+}
+
 function success(item) {
   if (item.enhancement >= 20) {
     return (item)
@@ -33,18 +45,6 @@ function fail(item) {
     })
     return {...item}
   }
-}
-
-function repair(item) {
-  // see Object.defineProperty() on MDN web docs page
-  // The arguments passed here to Object.defineProperty() are the object to be modified (item), the property on that object 
-  // to be defined/modified (durability), and the "descriptor" of the property (in this case, the value (100) to be assigned to the 
-  // "item" object's "durability" property).
-    Object.defineProperty(item, 'durability', {
-      value: 100
-    })
-    // return an item whose durability === 100
-    return {...item}
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,11 +1,17 @@
 const enhancer = require('./enhancer.js');
 
+// Items:
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
+
 describe("enhancer unit tests", () => {
 
     it("repairs", () => {
         expect(enhancer.repair({
             "name": "item",
             "enhancement": 10,
+            // The repair method accepts an item object and returns a new item object with the durability restored to 100. (see line 14)
             "durability": 50
         })).toEqual({
             "name": "item",
@@ -17,6 +23,7 @@ describe("enhancer unit tests", () => {
     it("enhances an item", () => {
         expect(enhancer.success({
             "name": "item",
+            // The success method increases the item's enhancement by 1. (see line 26)
             "enhancement": 19,
             "durability": 100
         })).toEqual({
@@ -29,6 +36,7 @@ describe("enhancer unit tests", () => {
     it("enhances an item", () => {
         expect(enhancer.success({
             "name": "item",
+            // If the item's enhancement level is 20, the enhancement level is not changed. (see line 39)
             "enhancement": 20,
             "durability": 100
         })).toEqual({
